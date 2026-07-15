f11dc1d0851c557b877b9ae234b2ee06584b7827
daniel-sd03 <dsodre2003@gmail.com>
2026-07-15 20:13:32 -0300

    fix: add missing security imports for AuthenticationPrincipal in QueueEntryController

--- src/main/java/sodresoftwares/barbearia/controllers/QueueEntryController.java
+++ src/main/java/sodresoftwares/barbearia/controllers/QueueEntryController.java
@@ -4,9 +4,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import sodresoftwares.barbearia.dto.JoinQueueDTO;
 import sodresoftwares.barbearia.dto.QueueEntryResponseDTO;
+import sodresoftwares.barbearia.model.user.User;
 import sodresoftwares.barbearia.services.QueueEntryService;
 
 @RestController
